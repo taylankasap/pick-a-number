5ddd77007c65152d3518ccf42d859395e0434b4e
Taylan Kasap <taylankasap@gmail.com>
2016-05-20 08:53:14 +0300

    Dev

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -44,6 +44,13 @@
                 var form = this;
                 var $form = $(form);
 
+                // Prevent multiple form submits at once
+                if ($form.attr('data-submitted')) {
+                    return false;
+                }
+
+                $form.attr('data-submitted', true);
+
                 if (form.checkValidity()) {
                     $.ajax({
                         'method': $form.attr('method'),
@@ -77,29 +84,35 @@
                 type: 'GET',
                 url: '/value-counts',
                 success: function(data) {
+                    data.sort(function(item1, item2) {
+                        if (item1.count === item2.count) {
+                            return 0;
+                        }
+
+                        return item1.count < item2.count ? 1 : -1;
+                    });
+
+                    var x = d3.scale.linear()
+                        .domain([0, data[0].count])
+                        .range([0, $(window).height()]);
+
                     d3.select('.canvas').selectAll('p')
                         .data(data)
                         .enter()
-                        .append('span')
-                        .style('display', 'block')
-                        .style('font-family', 'Courier')
+                        .append('div')
                         .html(function(item) {
-                            var value = item.value.toString();
-                            var count = item.count.toString();
-
-                            if (item.value <= 9) {
-                                value += '&nbsp;';
-                            }
-
-                            if (item.count <= 9) {
-                                count += '&nbsp;';
-                            }
-
-                            var text = 'Picked ' + value + ' ' + count + ' time' + (item.count > 1 ? 's' : '&nbsp;');
-
-                            text += '&nbsp;' + '-'.repeat(item.count);
-
-                            return text;
+                            return item.value;
+                        })
+                        .style('width', function(item) {
+                            return $(window).width() / data.length + 'px';
+                        })
+                        .style('background-color', function(item) {
+                            return next(colors);
+                        })
+                        .transition()
+                        .duration(2000)
+                        .style('height', function(item) {
+                            return x(item.count) + 'px';
                         });
                 }
             });
@@ -109,4 +122,36 @@
             'init': init
         };
     })();
+
+    var colors = [
+        '#1dd2af',
+        '#3498db',
+        '#c0392b',
+        '#9b59b6',
+        '#34495e',
+        '#2ecc71',
+        '#2980b9',
+        '#16a085',
+        '#7f8c8d',
+        '#8e44ad',
+        '#2c3e50',
+        '#f1c40f',
+        '#e67e22',
+        '#e74c3c',
+        '#27ae60',
+        '#95a5a6',
+        '#f39c12',
+        '#d35400',
+        '#bdc3c7'
+    ];
+
+    function next(arr) {
+        if (typeof this.cursor !== 'number' || this.cursor >= arr.length) {
+            this.cursor = 0;
+        } else {
+            this.cursor++;
+        }
+
+        return arr[this.cursor];
+    }
 })();
